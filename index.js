445242f0f7ce4f266c7e72ae22674fbfab933cc4
Houzifa Habbo <h-hebbo@hotmail.com>
2023-03-19 03:05:44 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(event = 'roller-skate'){
+    return `This Saturday, I want to ${event}!`;
+}
+const mondayWork = function(place = 'go to the office'){
+    return `This Monday, I will ${place}.`;
+}
+
+function wrapAdjective(visualFlair){
+    return function(adjective){
+        return `You are ${visualFlair}${adjective}${visualFlair}!`
+    }
+}
